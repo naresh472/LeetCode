414565cc8a69869ae1cb32cee2823c5d4965ec60
Naresh Teli <nareshteli185@gmail.com>
2025-02-22 16:19:53 +0530

    Time: 0 ms (100%), Space: 9 MB (39.66%) - LeetHub

--- 3372-longest-strictly-increasing-or-strictly-decreasing-subarray/3372-longest-strictly-increasing-or-strictly-decreasing-subarray.c
+++ 3372-longest-strictly-increasing-or-strictly-decreasing-subarray/3372-longest-strictly-increasing-or-strictly-decreasing-subarray.c
@@ -1,38 +1,28 @@
 int longestMonotonicSubarray(int* nums, int numsSize) {
 
     int max = 0,des=1,incre = 1;
-
+    if(numsSize == 1) return 1;
      for(int i = 0 ; i < numsSize - 1 ; i++)
      {
             if(nums[i] > nums[i+1])
             {
-                if(incre > 1)
-                {
-                    max = max < incre ? incre : max;
-                    incre = 1;
-                }
-
+                incre = 1;
                 des++;
             }
             else if(nums[i] < nums[i+1])
             {
-                if(des > 1)
-                {
-                   max = max < des ? des : max;
-                    des = 1;  
-                } 
+                des = 1;
                 incre++;
             }
             else
             {
-                int temp = incre > des ? incre : des ;
-                max = max < temp ? temp : max;
                 incre = 1;
                 des = 1;
             }
+            int temp = incre > des ? incre : des ;
+             max = max < temp ? temp : max;
      }
-       int temp = incre > des ? incre : des ;
-        max = max < temp ? temp : max;
+       
      return max;
     
 }

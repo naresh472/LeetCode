a74baeb2122c389fd05966f9e0798e3e1945e0aa
Naresh Teli <nareshteli185@gmail.com>
2025-02-22 15:48:08 +0530

    Time: 0 ms (100%), Space: 8.1 MB (30.84%) - LeetHub

--- 1878-check-if-array-is-sorted-and-rotated/1878-check-if-array-is-sorted-and-rotated.c
+++ 1878-check-if-array-is-sorted-and-rotated/1878-check-if-array-is-sorted-and-rotated.c
@@ -1,29 +1,12 @@
 bool check(int* nums, int numsSize) {
 
-    int last = 0,code = 0;
-    for (int i = 0 ; i < numsSize-1;i++)
+    int count = 0;
+    for (int i = 0 ; i < numsSize;i++)
     {
-        if(nums[i] < nums[i+1])
-        {
-             if(code == 1)
-            {
-                if(last > nums[i] && last < nums[i+1]) return false;
-
-            }
-        }
-        else if(nums[i] > nums[i+1])
-        {
-            if(code == 0) 
-            {
-                code = 1;
-                last = nums[i];
-                continue;
-            }
-            if(code == 1) return false;
-
-           
+       if (nums[i] > nums[(i + 1) % numsSize]) { 
+            count++;
         }
     }
-   return true;
+   return count <= 1;
     
 }
